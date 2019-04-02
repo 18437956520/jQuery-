4dfab1b3c5a3ce1b9514c5183bfc892b2eeca546
yyyh <444510796@qq.com>
2019-04-02 15:19:35 +0800

    浮动

--- main.js
+++ main.js
@@ -17,10 +17,23 @@ var size = allButtons.length
 allButtons.eq(n%size).trigger('click')//jQuery.eq 会获取dom然后自动封装为jQuery 4个一组循环
     .addClass('red')
     .siblings('.red').removeClass('red')
-setInterval(()=>{
+var timerId = setInterval(()=>{
     n += 1
     allButtons.eq(n%size).trigger('click')
     .addClass('red')
     .siblings('.red').removeClass('red')
 },1000)
 
+/******浮动暂停*******/
+$('.window').on('mouseenter', function(){
+    window.clearInterval(timerId)
+})
+/******离开开始*******/
+$('.window').on('mouseleave', function(){
+    timerId = setInterval(()=>{
+        n += 1
+        allButtons.eq(n%size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
+    },1000)
+})
