156a203de97f164840300215bf49ca426be1a366
yyyh <444510796@qq.com>
2019-04-02 15:32:32 +0800

    手动选择

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
 /******手动轮播*******/
 var allButtons = $('#buttons>button')
 
-for(let i=0; i<allButtons.length; i++){
-    $(allButtons[i]).on('click', function(x){
-        var index = $(x.currentTarget).index()//获取第几个button
-        var px = index * -1000//1000的倍数
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (x) {
+        var index = $(x.currentTarget).index() //获取第几个button
+        var px = index * -1000 //1000的倍数
         $('#images').css({
-            transform: 'translate('+px+'px)'
+            transform: 'translate(' + px + 'px)'
         })
+        n = index
+        allButtons.eq(n)
+            .addClass('red')
+            .siblings('.red').removeClass('red')
     })
 }
 
 /******自动轮播*******/
 var n = 0
 var size = allButtons.length
-allButtons.eq(n%size).trigger('click')//jQuery.eq 会获取dom然后自动封装为jQuery 4个一组循环
+allButtons.eq(n % size).trigger('click') //jQuery.eq 会获取dom然后自动封装为jQuery 4个一组循环
     .addClass('red')
     .siblings('.red').removeClass('red')
-var timerId = setInterval(()=>{
+var timerId = setInterval(() => {
     n += 1
-    allButtons.eq(n%size).trigger('click')
-    .addClass('red')
-    .siblings('.red').removeClass('red')
-},1000)
+    allButtons.eq(n % size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
+}, 2000)
 
 /******浮动暂停*******/
-$('.window').on('mouseenter', function(){
+$('.window').on('mouseenter', function () {
     window.clearInterval(timerId)
 })
 /******离开开始*******/
-$('.window').on('mouseleave', function(){
-    timerId = setInterval(()=>{
+$('.window').on('mouseleave', function () {
+    timerId = setInterval(() => {
         n += 1
-        allButtons.eq(n%size).trigger('click')
-        .addClass('red')
-        .siblings('.red').removeClass('red')
-    },1000)
+        allButtons.eq(n % size).trigger('click')
+            .addClass('red')
+            .siblings('.red').removeClass('red')
+    }, 2000)
 })
