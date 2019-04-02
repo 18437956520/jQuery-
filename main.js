9c5dac6e524f9e4059ecee69dedba0f63b960f8c
yyyh <444510796@qq.com>
2019-04-02 14:49:16 +0800

    jquery

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
+var allButtons = $('#buttons>button')
 
-$(p1).on('click', function () {
-    $(images).addClass('position-1')
-    .removeClass('position-2').removeClass('position-3').removeClass('position-4')
-})
-$(p2).on('click', function () {
-    $(images).addClass('position-2').removeClass('position-1').removeClass('position-3').removeClass('position-4')
-})
-$(p3).on('click', function () {
-    $(images).addClass('position-3').removeClass('position-1').removeClass('position-2').removeClass('position-4')
-})
-$(p4).on('click', function () {
-    $(images).addClass('position-4').removeClass('position-1').removeClass('position-2').removeClass('position-3')
-})
+for(let i=0; i<allButtons.length; i++){
+    $(allButtons[i]).on('click', function(x){
+        var index = $(x.currentTarget).index()//获取第几个button
+        var px = index * -1000//1000的倍数
+        $('#images').css({
+            transform: 'translate('+px+'px)'
+        })
+    })
+}
