b404152811f2076cb30b65e6ec7becd31f151bae
yyyh <444510796@qq.com>
2019-04-02 14:37:46 +0800

    分离(不推荐)

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 
 $(p1).on('click', function () {
-    $(images).css({
-        transform: 'translateX(0)'
-    })
+    $(images).addClass('position-1')
+    .removeClass('position-2').removeClass('position-3').removeClass('position-4')
 })
 $(p2).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-1000px)'
-    })
+    $(images).addClass('position-2').removeClass('position-1').removeClass('position-3').removeClass('position-4')
 })
 $(p3).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-2000px)'
-    })
+    $(images).addClass('position-3').removeClass('position-1').removeClass('position-2').removeClass('position-4')
 })
 $(p4).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-3000px)'
-    })
+    $(images).addClass('position-4').removeClass('position-1').removeClass('position-2').removeClass('position-3')
 })
